ce02ca0b9d436ea7ff9e097bf5f817c6b0719666
Phorce <phillipheels@googlemail.com>
2015-03-26 11:55:35 +0000

    Added files
    I have added these files

---
+++ src/Helper.h
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+
+typedef std::vector<double> Vector;
+
+namespace Helper {
+
+	float trace(std::vector<Vector> data); 
+	std::vector<Vector> multiply_matrix(std::vector<Vector> matrix1, 
+					    std::vector<Vector> matrix2); 
+	
+
+}
---
+++ src/PCA.h
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <vector>
+#include "Cov.h"
+#include "Eigen.h"
+typedef std::vector<double> Vector; 
+
+
+class PCA {
+
+	public: 
+
+	   PCA(); 
+
+	   template<typename T>
+	   PCA(T begin, T end) 
+	   : data(begin, end)
+	   {
+		c = Cov(begin, end); 
+
+		Solve();
+	   }
+
+	   std::vector<Vector> cov(); 
+		
+	   std::vector<Vector> getEigen(); 
+	   
+	   std::vector<Vector>
+
+	   void Solve();
+
+	   friend std::ostream& operator <<(std::ostream&, PCA& a); 
+	private:
+	  Cov c;
+	  std::vector<Vector> cov_matrix; 
+	  std::vector<Vector> data; 
+	  std::vector<Vector> values; 
+
+}; 
+
+
+
+
+
